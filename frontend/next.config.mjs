348e9fd74980a52ccd76a39f69196d815a9a3a1b
anisouared <ouared.anis@gmail.com>
2024-11-15 18:48:16 +0100

    Disable ESLint during build

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true, // Désactive ESLint pendant le build
+    },
     // experimental: {
     //     appDir: true,
     // },
