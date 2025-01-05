aef1bbf743ac1c4cdb61504ef51a35d306c1a651
anisouared <ouared.anis@gmail.com>
2025-01-05 20:57:47 +0100

    Deleting some comments

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -12,16 +12,6 @@ const config: HardhatUserConfig = {
   solidity: "0.8.20",
   networks: {
     hardhat: {},
-    // sepolia: {
-    //   url: process.env.INFURA_SEPOLIA_RPC_URL,
-    //   accounts: [`0x${process.env.PRIVATE_KEY}`],
-    //   chainId: 11155111,
-    // },
-    // holesky: {
-    //   url: process.env.INFURA_HOLESKY_RPC_URL,
-    //   accounts: [`0x${process.env.PRIVATE_KEY}`],
-    //   chainId: 17000,
-    // },
     localhost: {
       url: "http://127.0.0.1:8545",
       chainId: 31337,
