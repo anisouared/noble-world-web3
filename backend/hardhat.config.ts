3bc441c3291d16712d0169862dc6b95654779a40
anisouared <ouared.anis@gmail.com>
2025-01-05 20:19:36 +0100

    Create backend-tests YML file

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -7,41 +7,46 @@ import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-chai-matchers";
 
 dotenvConfig();
-const PRIVATE_KEY_LOCALHOST_HARDHAT = "0000000000000000000000000000000000000000000000000000000000000000";
-const PRIVATE_KEY_BASE_SEPOLIA =
-  `0x${process.env.PRIVATE_KEY}` || "0000000000000000000000000000000000000000000000000000000000000000";
-const PRIVATE_KEY_BASE_HOLESKY =
-  `0x${process.env.PRIVATE_KEY}` || "0000000000000000000000000000000000000000000000000000000000000000";
-const INFURA_SEPOLIA_RPC_URL =
-  process.env.INFURA_SEPOLIA_RPC_URL || "0000000000000000000000000000000000000000000000000000000000000000";
-const INFURA_HOLESKY_RPC_URL =
-  process.env.INFURA_HOLESKY_RPC_URL || "0000000000000000000000000000000000000000000000000000000000000000";
-
-let PRIVATE_KEY = process.env.PRIVATE_KEY || "";
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
   networks: {
     hardhat: {},
-    sepolia: {
-      url: INFURA_SEPOLIA_RPC_URL,
-      accounts: [PRIVATE_KEY_BASE_SEPOLIA],
-      chainId: 11155111,
-    },
-    holesky: {
-      url: INFURA_HOLESKY_RPC_URL,
-      accounts: [PRIVATE_KEY_BASE_HOLESKY],
-      chainId: 17000,
-    },
+    // sepolia: {
+    //   url: process.env.INFURA_SEPOLIA_RPC_URL,
+    //   accounts: [`0x${process.env.PRIVATE_KEY}`],
+    //   chainId: 11155111,
+    // },
+    // holesky: {
+    //   url: process.env.INFURA_HOLESKY_RPC_URL,
+    //   accounts: [`0x${process.env.PRIVATE_KEY}`],
+    //   chainId: 17000,
+    // },
     localhost: {
       url: "http://127.0.0.1:8545",
       chainId: 31337,
     },
   },
   etherscan: {
-    apiKey: ETHERSCAN_API_KEY,
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
+
+if (
+  process.env.PRIVATE_KEY &&
+  process.env.PRIVATE_KEY.length === 64 &&
+  process.env.INFURA_HOLESKY_RPC_URL &&
+  process.env.ETHERSCAN_API_KEY
+) {
+  config.networks.holesky = {
+    url: process.env.INFURA_HOLESKY_RPC_URL,
+    accounts: [`0x${process.env.PRIVATE_KEY}`],
+    chainId: 17000,
+  };
+
+  config.etherscan = {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  };
+}
 
 export default config;
